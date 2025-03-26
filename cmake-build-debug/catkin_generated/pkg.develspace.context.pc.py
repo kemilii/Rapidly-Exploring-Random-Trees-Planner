f6a7065035c2e5c291648f1f071feebd61f2fda7
Jiali <79980461+kemilii@users.noreply.github.com>
2025-03-26 14:35:01 +0800

    First commit

---
+++ cmake-build-debug/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/naivehobo/Desktop/polybee/misc/RRT-Assessment/Polybee-ROS-RRT-Task/rrt_planner_ros/include".split(';') if "/home/naivehobo/Desktop/polybee/misc/RRT-Assessment/Polybee-ROS-RRT-Task/rrt_planner_ros/include" != "" else []
+PROJECT_CATKIN_DEPENDS = "nav_msgs;geometry_msgs;roscpp".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lrrt_planner".split(';') if "-lrrt_planner" != "" else []
+PROJECT_NAME = "rrt_planner"
+PROJECT_SPACE_DIR = "/home/naivehobo/Desktop/polybee/misc/RRT-Assessment/Polybee-ROS-RRT-Task/rrt_planner_ros/cmake-build-debug/devel"
+PROJECT_VERSION = "0.0.0"
---
+++ cmake-build-debug/catkin_generated/pkg.installspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "${prefix}/include".split(';') if "${prefix}/include" != "" else []
+PROJECT_CATKIN_DEPENDS = "nav_msgs;geometry_msgs;roscpp".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lrrt_planner".split(';') if "-lrrt_planner" != "" else []
+PROJECT_NAME = "rrt_planner"
+PROJECT_SPACE_DIR = "/usr/local"
+PROJECT_VERSION = "0.0.0"
